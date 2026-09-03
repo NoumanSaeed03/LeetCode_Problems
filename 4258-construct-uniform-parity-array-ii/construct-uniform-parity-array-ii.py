9615d0a4289fb5dd97f7376ce8a6de7c5d09fef8
Nouman Saeed <snouman469@gmail.com>
2026-09-03 23:41:37 +0500

    Time: 27 ms (75.61%) | Memory: 35.3 MB (73.17%) - LeetSync

--- 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -1,10 +1,9 @@
 class Solution:
-    def uniformArray(self, a):
-        mn = float('inf')
-        oddCnt = 0
-        for x in a:
-            mn = min(mn, x)
-            if x % 2 == 1:
-                oddCnt += 1
-        # min Element is ODD(remaining even > min) or All Even!
-        return mn % 2 == 1 or oddCnt == 0
+    def uniformArray(self, nums1: list[int]) -> bool:
+        m=min(nums1)
+        if m&1:
+            return True
+        for n in nums1:
+            if n&1:
+                return False
+        return True
